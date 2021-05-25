8d7594b9ae2388423ff595c6ae3961df1cf7e5a3
Alex <aekuz89@gmail.com>
2021-05-25 14:19:34 +0700

    сверстан блок _product-card

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,20 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.png"></button>',
     });
+
+    $('.product-card__more').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.product-card__content').eq(i).toggleClass('product-card__content--active');
+            $('.product-card__feature').eq(i).toggleClass('product-card__feature--active');
+        })
+    })
+
+    $('.product-card__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.product-card__content').eq(i).toggleClass('product-card__content--active');
+            $('.product-card__feature').eq(i).toggleClass('product-card__feature--active');
+        })
+    })
   });
